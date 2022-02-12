7063855fa77d1cf5b0b936734c42b43e0169d829
Doki-Mani-1999 <manikantadoki1999@gmail.com>
2022-02-12 17:21:27 +0530

    done

---
+++ 3_Implementation/bank.h
@@ -0,0 +1,18 @@
+/**
+ * @file bank.c
+ * @author Manikanta Doki (manikantadoki1999@gmail.com)
+ * @brief Project on Banking System
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// Header gaurd
+#ifndef SNAKE_AND_LADDER_H_
+#define SNAKE_AND_LADDER_H_
+
+#include <stdio.h>
+
+#endif
